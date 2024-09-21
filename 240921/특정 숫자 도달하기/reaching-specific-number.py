a822e1a89b6c32518a70646166c51ea670a532df
gilbertzzang0701 <PKY7293@gmail.com>
2024-09-21 13:17:32 +0900

    Title: 특정 숫자 도달하기, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240921/특정 숫자 도달하기/reaching-specific-number.py
+++ 240921/특정 숫자 도달하기/reaching-specific-number.py
@@ -1,7 +1,9 @@
 sum=0
+q=0
 a = list(map(int, input().split()))
 for i in range(len(a)):
     if a[i]>=250:
         break
     sum+=int(a[i])
-print(sum,'%.1f' %(sum/5))
+    q+=1
+print(sum,'%.1f' %(sum/q))
