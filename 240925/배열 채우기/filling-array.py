31a66fc1c5e18bc63114d50c41a0fada58815123
gilbertzzang0701 <PKY7293@gmail.com>
2024-09-25 16:45:46 +0900

    Title: 배열 채우기, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240925/배열 채우기/filling-array.py
+++ 240925/배열 채우기/filling-array.py
@@ -1,5 +1,11 @@
 arr = list(map(int, input().split()))
-for i in range(len(arr)-1,-1,-1):
+zero=0
+idx=-1
+for i in range(len(arr)):
     if arr[i]==0:
+        idx=i
+        break
+for i in range(len(arr)-1,-1,-1):
+    if arr[idx]==0:
         continue
     print(arr[i],end=' ')
