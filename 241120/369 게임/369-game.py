e3095eea729b08004250015fb65f6d99f2dfe177
gilbertzzang0701 <PKY7293@gmail.com>
2024-11-20 16:04:18 +0900

    Title: 369 게임, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 241120/369 게임/369-game.py
+++ 241120/369 게임/369-game.py
@@ -1,8 +1,7 @@
 n=input()
 n=int(n)
 for i in range(1,n+1):
-    #
-    if i%3==1 or '3' in str(i) or '6' in str(i) or '9' in str(i):
-        print(0)
+    if i%3==0 or '3' in str(i) or '6' in str(i) or '9' in str(i):
+        print(0,end=' ')
     else:
-        print(i)
+        print(i,end=' ')
