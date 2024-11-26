6265d4c8e507f800119d00ef5251fea38f596394
gilbertzzang0701 <PKY7293@gmail.com>
2024-11-26 17:02:21 +0900

    Title: 소수 판별기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241126/소수 판별기/prime-number-discriminator.py
+++ 241126/소수 판별기/prime-number-discriminator.py
@@ -2,10 +2,11 @@ s=0
 n=input()
 n=int(n)
 for i in range(1,n+1):
-    if n%1== 0 and n%n==0:
+    if n%i== 0:
         s='P'
+        
     else:
         s='C'
-        print(i)
-        break
+
+
 print(s)
