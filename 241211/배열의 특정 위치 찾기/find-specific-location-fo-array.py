a4eb8d33c68cef0a1ec19a66d67948ee79f8645e
gilbertzzang0701 <PKY7293@gmail.com>
2024-12-11 18:12:38 +0900

    Title: 배열의 특정 위치 찾기, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 241211/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ 241211/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -1,12 +1,12 @@
-cnt=0
-a=0
-b=0
-arr = list(map(int,input().split()))
-for i in range(len(arr)):
-    #짝수 값만 합계를 구한다
-    if arr[i]%2==0:
-        cnt+=arr[i]
+# 입력받은 10개의 정수
+numbers = list(map(int, input().split()))
 
-    #짝수 위치의 합계를 구한다.
-    #if
-print(cnt)
+# 짝수 번째 원소들의 합 (인덱스 1, 3, 5, 7, 9)
+even_sum = sum(numbers[i] for i in range(1, 10, 2))
+
+# 3의 배수 번째 원소들의 평균 (인덱스 2, 5, 8)
+multiples_of_three = [numbers[i] for i in range(2, 10, 3)]
+average_of_multiples_of_three = round(sum(multiples_of_three) / len(multiples_of_three), 1)
+
+# 결과 출력
+print(even_sum, average_of_multiples_of_three)
