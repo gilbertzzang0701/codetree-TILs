9e152236e7519607314571c21b09b5ad41c4d481
gilbertzzang0701 <PKY7293@gmail.com>
2024-12-23 16:32:16 +0900

    Title: 배열 채우기, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241223/배열 채우기/filling-array.py
@@ -0,0 +1,16 @@
+arr = list(map(int, input().split()))
+for i in range(len(arr)):
+    if arr[i]==0:
+        for j in range(1,len(arr)): # 5
+            print(arr[i-j],end=' ')   # i = 5 , j = 0 -> 5 -> 4
+        break
+
+# for i in range( 4 ,-1)
+
+# 0 1  2  3 4 5
+# 3 1 13 5 83 0
+
+#찾는 과정 : 5
+# 출력 결과 : 
+# 4 3 2 1 0
+# 83 5 13 1 3
