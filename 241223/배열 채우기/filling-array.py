d318c4ed4e6249c6392a5d75c0b7591f7a28e646
gilbertzzang0701 <PKY7293@gmail.com>
2024-12-23 16:57:14 +0900

    Title: 배열 채우기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241223/배열 채우기/filling-array.py
+++ 241223/배열 채우기/filling-array.py
@@ -3,7 +3,8 @@ a=0
 for i in range(len(arr)):
     if arr[i]==0:
         for j in range(1,len(arr)): # 5
-            print(arr[i-j],end=' ')   # i = 5 , j = 0 -> 5 -> 4
+            print(arr[i-j],end=' ')
+        a=0
         break
     else:
         a=1
